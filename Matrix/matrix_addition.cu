1cb4511d38b56ee152c5dc8ccca01ecd6685c1f8
Ajay Singh Pawar <40195244+ajayseeker@users.noreply.github.com>
2020-04-03 11:58:07 +0530

    Update and rename matMul.cu to matrix_addition.cu

--- Matrix/matrix_addition.cu
+++ Matrix/matrix_addition.cu
@@ -4,13 +4,10 @@
 #define BlockSize (8)
 #define n         (8)
 
-void printMatrix(int *a)
-{
+void printMatrix(int *a){
 
-	for(int i=0; i<n; i++)
-	{
-		for(int j=0; j<n; j++)
-		{
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
 			printf("%d ",a[i*n+j]);
 		}
 		printf("\n");
@@ -18,8 +15,7 @@ void printMatrix(int *a)
 	printf("\n");
 }
 
-__global__ void matrixMulKernel(int *da, int *db, int *dc)
-{
+__global__ void matrixMulKernel(int *da, int *db, int *dc){
 	int row=blockIdx.y*blockDim.y+threadIdx.y;
 	int col=blockIdx.x*blockDim.x+threadIdx.x;
 
@@ -27,8 +23,7 @@ __global__ void matrixMulKernel(int *da, int *db, int *dc)
 		dc[row*n+col]=da[row*n+col]+db[row*n+col];
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
 	int *a, *b, *c;
 	int size;
 	int *da, *db, *dc;
@@ -39,57 +34,49 @@ int main(int argc, char **argv)
 
 	size=n*n;
 	a=(int *)calloc(size, sizeof(int));
-	if(a==NULL)
-	{
+	if(a==NULL){
 		printf("Error while dynamically allocating matrix A\n");
 	}
 
 	b=(int *)calloc(size, sizeof(int));
-	if(b==NULL)
-	{
+	
+	if(b==NULL){
 		printf("Error while dynamically allocating matrix B\n");
 	}
 
 	c=(int *)calloc(size, sizeof(int));
-	if(c==NULL)
-	{
+	
+	if(c==NULL){
 		printf("Error while dynamically allocating matrix C\n");
 	}
 	
-	for(int i=0; i<size; i++)
-	{
+	for(int i=0; i<size; i++){
 		a[i]=2;
 		b[i]=2;
 	}
 	err=cudaMalloc((void **)&da, csize);
-	if(err!=cudaSuccess)
-	{
+	if(err!=cudaSuccess){
 		printf("Error while allocating memory for matrix A using cudamalloc\n");
 		printf("error string: %s\n",cudaGetErrorString(err));
 	}
-	if(cudaMemcpy(da, a, size*sizeof(int), cudaMemcpyHostToDevice)!=cudaSuccess)
-	{
+	if(cudaMemcpy(da, a, size*sizeof(int), cudaMemcpyHostToDevice)!=cudaSuccess){
 		printf("Error in cudaMemCpy while transferring a\n");
 		return 0;
 	}
 	
-	if(cudaMalloc((void **)&db, csize)!=cudaSuccess)
-	{
+	if(cudaMalloc((void **)&db, csize)!=cudaSuccess){
 		printf("Error while allocating memory for matrix B using cudamalloc\n");
 	}
-	if(cudaMemcpy(db, b, size*sizeof(int), cudaMemcpyHostToDevice)!=cudaSuccess)
-	{
+	if(cudaMemcpy(db, b, size*sizeof(int), cudaMemcpyHostToDevice)!=cudaSuccess){
 		printf("Error in cudaMemCpy while transferring b\n");
 		return 0;
 	}
 	
-	if(cudaMalloc((void **)&dc, csize)!=cudaSuccess)
-	{
+	if(cudaMalloc((void **)&dc, csize)!=cudaSuccess){
 		printf("Error while allocating memory for matrix C using cudamalloc\n");
 	}
 	matrixMulKernel<<<grid, block>>>(da, db, dc);
-	if(cudaMemcpy(dc, c, size*sizeof(int), cudaMemcpyDeviceToHost)!=cudaSuccess)
-	{
+	if(cudaMemcpy(dc, c, size*sizeof(int), cudaMemcpyDeviceToHost)!=cudaSuccess){
 		printf("Error in cudaMemCpy while transferring c\n");
 		return 0;
 	}
